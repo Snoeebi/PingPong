36a08a4b8884c8f6900f64f68df3bf2505b86543
Snoeebi <17sebastian10@gmail.com>
2019-02-26 15:55:59 +0100

    - V1 Umgesetzt

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PingPong
+{
+    partial class frmPingPong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.ovsBall = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.pnlSpiel = new System.Windows.Forms.Panel();
+            this.shapeContainer2 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.tmrSpiel = new System.Windows.Forms.Timer(this.components);
+            this.pnlSpiel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(355, 374);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(122, 36);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Spiel starten";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ovsBall
+            // 
+            this.ovsBall.BackColor = System.Drawing.Color.Yellow;
+            this.ovsBall.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovsBall.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.ovsBall.Location = new System.Drawing.Point(121, 72);
+            this.ovsBall.Name = "ovsBall";
+            this.ovsBall.Size = new System.Drawing.Size(50, 50);
+            // 
+            // pnlSpiel
+            // 
+            this.pnlSpiel.BackColor = System.Drawing.Color.SeaGreen;
+            this.pnlSpiel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSpiel.Controls.Add(this.shapeContainer2);
+            this.pnlSpiel.Location = new System.Drawing.Point(6, 6);
+            this.pnlSpiel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlSpiel.Name = "pnlSpiel";
+            this.pnlSpiel.Size = new System.Drawing.Size(735, 336);
+            this.pnlSpiel.TabIndex = 2;
+            // 
+            // shapeContainer2
+            // 
+            this.shapeContainer2.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer2.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer2.Name = "shapeContainer2";
+            this.shapeContainer2.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovsBall});
+            this.shapeContainer2.Size = new System.Drawing.Size(733, 334);
+            this.shapeContainer2.TabIndex = 0;
+            this.shapeContainer2.TabStop = false;
+            // 
+            // tmrSpiel
+            // 
+            this.tmrSpiel.Interval = 120;
+            this.tmrSpiel.Tick += new System.EventHandler(this.tmrSpiel_Tick);
+            // 
+            // frmPingPong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 499);
+            this.Controls.Add(this.pnlSpiel);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPingPong";
+            this.Text = "Ping-Pong Spiel";
+            this.pnlSpiel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovsBall;
+        private System.Windows.Forms.Panel pnlSpiel;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer2;
+        private System.Windows.Forms.Timer tmrSpiel;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class frmPingPong : Form
+    {
+        private const int CHANGEX = 5;
+        private const int CHANGEY = 2;
+
+        private int _panelMaxY;
+        private int _panelMaxX;
+        private Operators _xDirection = Operators.PLUS;
+        private Operators _yDirection = Operators.PLUS;
+
+        public frmPingPong()
+        {
+            InitializeComponent();
+
+            _panelMaxY = pnlSpiel.Size.Height;
+            _panelMaxX = pnlSpiel.Size.Width;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrSpiel.Start();
+        }
+
+        private void tmrSpiel_Tick(object sender, EventArgs e)
+        {
+            Point newBallPoint = GetBallNewBallPointByLocation();
+
+            ovsBall.Location = newBallPoint;
+        }
+
+        private Point GetBallNewBallPointByLocation()
+        {
+            Point newBallPoint;
+
+            if(_yDirection == Operators.PLUS)
+            {
+                if(_xDirection == Operators.PLUS)
+                {
+                    newBallPoint = new Point(ovsBall.Location.X + CHANGEX, ovsBall.Location.Y + CHANGEY);
+                }
+                else
+                {
+                    newBallPoint = new Point(ovsBall.Location.X - CHANGEX, ovsBall.Location.Y + CHANGEY);
+                }
+            }
+            else
+            {
+                if(_xDirection == Operators.PLUS)
+                {
+                    newBallPoint = new Point(ovsBall.Location.X + CHANGEX, ovsBall.Location.Y - CHANGEY);
+                }
+                else
+                {
+                    newBallPoint = new Point(ovsBall.Location.X - CHANGEX, ovsBall.Location.Y - CHANGEY);
+                }
+            }
+
+            if (ovsBall.Location.Y + ovsBall.Height >= _panelMaxY)
+            {
+                // BALL KOMMT UNTEN AN
+                if (_xDirection == Operators.PLUS)
+                {
+                    newBallPoint = new Point(ovsBall.Location.X + CHANGEX, ovsBall.Location.Y - CHANGEY);
+                    _yDirection = Operators.MINUS;
+                }
+                else
+                {
+                    newBallPoint = new Point(ovsBall.Location.X - CHANGEX, ovsBall.Location.Y - CHANGEY);
+                    _yDirection = Operators.MINUS;
+                }
+            }
+            else if (ovsBall.Location.X + ovsBall.Width >= _panelMaxX)
+            {
+                // BALL KOMMT RECHTS AN
+                if (_yDirection == Operators.PLUS)
+                {
+                    newBallPoint = new Point(ovsBall.Location.X - CHANGEX, ovsBall.Location.Y + CHANGEY);
+                    _xDirection = Operators.MINUS;
+                }
+                else
+                {
+                    newBallPoint = new Point(ovsBall.Location.X - CHANGEX, ovsBall.Location.Y - CHANGEY);
+                    _xDirection = Operators.MINUS;
+                }
+            }
+            else if (ovsBall.Location.Y <= pnlSpiel.Location.Y)
+            {
+                // BALL KOMMT OBEN AN
+                if (_xDirection == Operators.PLUS)
+                {
+                    newBallPoint = new Point(ovsBall.Location.X + CHANGEX, ovsBall.Location.Y + CHANGEY);
+                    _yDirection = Operators.PLUS;
+                }
+                else
+                {
+                    newBallPoint = new Point(ovsBall.Location.X - CHANGEX, ovsBall.Location.Y + CHANGEY);
+                    _yDirection = Operators.PLUS;
+                }
+            }
+            else if (ovsBall.Location.X <= pnlSpiel.Location.X)
+            {
+                // BALL KOMMT LINKS AN
+                if (_yDirection == Operators.PLUS)
+                {
+                    newBallPoint = new Point(ovsBall.Location.X + CHANGEX, ovsBall.Location.Y + CHANGEY);
+                    _xDirection = Operators.PLUS;
+                }
+                else
+                {
+                    newBallPoint = new Point(ovsBall.Location.X + CHANGEX, ovsBall.Location.Y - CHANGEY);
+                    _xDirection = Operators.PLUS;
+                }
+            }
+
+            return newBallPoint;
+        }
+    }
+}
